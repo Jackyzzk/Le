29852681aea63f9927a4ef5a35503ba865bd5037
zhangzheke <zhangzheke@maetimes.com>
2024-03-13 03:24:56 +0800

    a

--- 哈希表/9.最长连续序列.go
+++ 哈希表/9.最长连续序列.go
@@ -27,25 +27,38 @@ https://leetcode.cn/problems/longest-consecutive-sequence/description/
 
 func longestConsecutive(nums []int) int {
 	num2cnt := make(map[int]int, len(nums))
-	num2next := make(map[int]int, len(nums))
+	var ret int
 	for _, x := range nums {
-		if v, ok := num2cnt[x-1]; ok {
-			num2cnt[x-1] = v + 1
+		if _, ok := num2cnt[x]; ok {
 			continue
 		}
-		if v, ok := num2cnt[x+1]; ok {
-
+		length := 1
+		// 如果 x-1存在，在x来之前，x-1一定是右端点
+		if v, ok := num2cnt[x-1]; ok {
+			length += v
 		}
-	}
-	var ret int
-	for _, x := range num2cnt {
-		ret = max(ret, x)
+		// 如果 x+1存在，在x来之前，x+1一定是左端点
+		if v, ok := num2cnt[x+1]; ok {
+			length += v
+		}
+		// l = right - left + 1
+		if v, ok := num2cnt[x-1]; ok {
+			// left = right + 1 - l
+			num2cnt[x-v] = length
+		}
+		if v, ok := num2cnt[x+1]; ok {
+			// right = l + left - 1
+			num2cnt[v+x] = length
+		}
+		num2cnt[x] = length
+		ret = max(ret, length)
 	}
 	return ret
 }
 
 func main() {
-	nums := []int{100, 4, 200, 1, 3, 2}
+	//nums := []int{100, 4, 200, 1, 3, 2}
+	nums := []int{0, 3, 7, 2, 5, 8, 4, 6, 0, 1}
 	res := longestConsecutive(nums)
 	fmt.Println(res)
 }
