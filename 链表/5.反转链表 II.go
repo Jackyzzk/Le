db63f3c6469ac287960486e0bdfbc7788d321747
Jackyzzk <417759979@qq.com>
2024-04-06 09:37:33 +0800

    a

---
+++ 链表/5.反转链表 II.go
@@ -0,0 +1,52 @@
+package main
+
+/*
+给你单链表的头指针 head 和两个整数 left 和 right ，其中 left <= right 。
+请你反转从位置 left 到位置 right 的链表节点，返回 反转后的链表 。
+
+示例 1：
+
+输入：head = [1,2,3,4,5], left = 2, right = 4
+输出：[1,4,3,2,5]
+示例 2：
+
+输入：head = [5], left = 1, right = 1
+输出：[5]
+
+提示：
+
+链表中节点数目为 n
+1 <= n <= 500
+-500 <= Node.val <= 500
+1 <= left <= right <= n
+
+进阶： 你可以使用一趟扫描完成反转吗？
+
+https://leetcode.cn/problems/reverse-linked-list-ii/description/
+*/
+
+func reverseBetween(head *ListNode, left int, right int) *ListNode {
+	if left == right {
+		return head
+	}
+	h := head
+	ret := &ListNode{Next: head}
+	var pre *ListNode
+	preLeftN, leftN := ret, head
+
+	for i := 1; h != nil && i <= right; i++ {
+		if i < left {
+			preLeftN, h = h, h.Next
+			continue
+		}
+		if i == left {
+			leftN = h
+		}
+		if i == right {
+			preLeftN.Next, leftN.Next = h, h.Next
+		}
+
+		pre, h.Next, h = h, pre, h.Next
+	}
+	return ret.Next
+}
